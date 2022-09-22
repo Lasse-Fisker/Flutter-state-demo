fefacbe43f8bcaacbe357fff3307c7666d702914
Lasse Fisker <lnf@it-minds.dk>
2022-09-22 17:19:30 +0200

    Add Main layout

--- lib/layout.dart
+++ lib/layout.dart
@@ -15,25 +15,23 @@ class Layout extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Spacer(),
-          Text(
-            title,
-            textAlign: TextAlign.center,
-            style: theme.textTheme.headlineSmall,
-          ),
-          const Spacer(),
-          const Spacer(),
-          counter,
-          const Spacer(),
-          actions,
-          const Spacer(),
-          const Spacer()
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Spacer(),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: theme.textTheme.headlineSmall,
+        ),
+        const Spacer(),
+        const Spacer(),
+        counter,
+        const Spacer(),
+        actions,
+        const Spacer(),
+        const Spacer()
+      ],
     );
   }
 }
